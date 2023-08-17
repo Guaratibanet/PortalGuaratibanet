8007a4eafb07bc09de07ef795290e34899412945
Guaratibanet <136260571+Guaratibanet@users.noreply.github.com>
2023-08-17 15:52:29 -0300

    Update script.js

--- javascript/script.js
+++ javascript/script.js
@@ -1,1 +1,33 @@
-//alert ('Manutenção: SPR037CX076. Previsão: 18h')
+class GuaratibaNet {
+
+    constructor() {
+
+        this.inputSearch = document.querySelector ("#input-search-id")
+        this.buttonSearch = document.querySelector ("#button-search-id")
+        
+        this.buttonSearch.addEventListener ("click", () => {
+            this.searchFunc()
+        })
+
+        alert ("Manutenção: BSR048CX554 / BSR048CX555. Previsão: 18h")
+
+    }
+
+    searchFunc() {
+
+        this.inputSearchValue = this.inputSearch.value
+
+        if (this.inputSearchValue === '') {
+            alert ("ins")
+        } else {
+            console.log (this.inputSearchValue)
+            console.log (typeof this.inputSearchValue)
+        }
+
+        
+    }
+
+}
+
+
+const guaratibaNet = new GuaratibaNet()
