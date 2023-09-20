bb2b25bdea75d653e64b57273865c028f6395788
Guaratibanet <136260571+Guaratibanet@users.noreply.github.com>
2023-09-20 15:51:56 -0300

    Update script.js
    Js - search

--- javascript/script.js
+++ javascript/script.js
@@ -1,36 +1,18 @@
-export default class GuaratibaNet {
+class GuaratibaNet {
 
     constructor() {
 
-        this.inputSearch = document.querySelector ("#input-search-id")
         this.buttonSearch = document.querySelector ("#button-search-id")
         
         this.buttonSearch.addEventListener ("click", () => {
             this.searchFunc()
         })
 
-        //alert ("Manutenção: BSR048CX554 / BSR048CX555. Previsão: 18h")
-
     }
 
     searchFunc() {
-
-        this.inputSearchValue = this.inputSearch.value
-        this.inputSearchValue = this.inputSearchValue.trim()
-
-        if (this.inputSearchValue === '') {
-            alert ("Insira as informações no campo de busca")
-            this.inputSearch.value = ""
-        } else {
-            console.log (this.inputSearchValue)
-            //window.location.href = "pages/index-searchlist.html"
-        }
-
-        
+        location.href = "pages/index-searchlist.html"
         
     }
 
 }
-
-
-const guaratibaNet = new GuaratibaNet()
